0a1e6b1ae60fd1053be1f2722cd0b2d4ea0dba26
Abby Reynolds <avreynol@ncsu.edu>
2025-05-21 20:17:09 -0400

    refactor to see if this fixes it.

--- app.py
+++ app.py
@@ -55,6 +55,5 @@ def openapi_spec():
     return send_file("openapi.yaml", mimetype="text/yaml")
 
 if __name__ == "__main__":
-    """Uncomment below for local"""
-    # app.run(debug=True)
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
